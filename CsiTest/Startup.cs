e590371f916a6463a662422fbe964281b7cdc023
Ian <ian@mobedia.co>
2017-02-17 17:08:44 +0000

    Add project files.

---
+++ CsiTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CsiTest.Startup))]
+namespace CsiTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
