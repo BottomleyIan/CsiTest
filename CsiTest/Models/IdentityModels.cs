720586c7d2cfa15aea40f251e3df7d186588dc95
Ian <ian@mobedia.co>
2017-02-17 17:22:58 +0000

    Added Database and initial Model for storing the random numbers

--- CsiTest/Models/IdentityModels.cs
+++ CsiTest/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace CsiTest.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<RandomNumber> RandomNumbers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
---
+++ CsiTest/Models/RandomNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CsiTest.Models
+{
+    public class RandomNumber
+    {
+        public int Id { get; set; }
+        public int Millis { get; set; }
+        public string SortedNumbers { get; set; }
+        public bool Ascending { get; set; }
+    }
+}
